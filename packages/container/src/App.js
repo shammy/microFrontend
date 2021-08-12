62a4dc8488e8d66de80f2cbfe3ffc7693bca6581
sdhuri <sdhuri@getwellnetwork.com>
2021-08-12 11:12:31 -0400

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './component/MarketingApp';
+
+export default () => {
+  return (
+    <div>
+      <h1>container</h1>
+      <hr/>
+      <MarketingApp />
+    </div>
+  );
+};
