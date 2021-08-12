74ba6f97c42fcbfbcfe5806747fc9991b30d8bdf
sdhuri <sdhuri@getwellnetwork.com>
2021-08-12 19:17:55 -0400

    test

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -7,18 +7,18 @@ const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: 'production',
-  output:{
-    filename: '[name].[contenthash].js'
+  output: {
+    filename: '[name].[contenthash].js',
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
-      remotes:{
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
-      shared: packageJson.dependencies
-    })
-  ]
-}
-
-module.exports = merge(commonConfig, prodConfig)
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+//prod
+module.exports = merge(commonConfig, prodConfig);
