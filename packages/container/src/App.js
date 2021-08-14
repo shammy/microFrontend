d916148e8774f5cfe8636a1c14200281a949e26a
sdhuri <sdhuri@getwellnetwork.com>
2021-08-14 15:37:10 -0400

    lazyloading subapps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import Header from './components/Header';
-import AuthApp from './components/AuthApp';
-import MarketingApp from './components/MarketingApp';
+import Progress from './components/Progress';
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
@@ -15,10 +16,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
