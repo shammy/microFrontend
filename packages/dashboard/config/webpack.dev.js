247a427ba74e6311758f4e7b07d41bb8c2d76db5
sdhuri <sdhuri@getwellnetwork.com>
2021-08-14 16:48:39 -0400

    vue dashboard app

--- packages/dashboard/config/webpack.dev.js
+++ packages/dashboard/config/webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -6,23 +7,28 @@ const packageJson = require('../package.json');
 const devConfig = {
   mode: 'development',
   output: {
-    publicPath: 'http://localhost:8083/',
+    publicPath: 'http://localhost:8085/',
   },
   devServer: {
-    port: 8083,
+    port: 8085,
     historyApiFallback: {
       index: 'index.html',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        auth: 'auth@http://localhost:8084/remoteEntry.js',
-        dashboard: 'dashboard@http://localhost:8085/remoteEntry.js',
-        marketing: 'marketing@http://localhost:8082/remoteEntry.js',
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DashboardApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
     }),
   ],
 };
